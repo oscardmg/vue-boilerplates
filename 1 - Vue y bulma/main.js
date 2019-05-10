26727a9d2906ce32aee0d674f51716f0e889fe65
Oscar Montoya <oscardmg@gmail.com>
2019-05-10 16:03:45 -0500

    Primer commit

---
+++ 1 - Vue y bulma/main.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#app',
+  data: {
+    title: 'Vue Boilerplate con bulma y fontawesome',
+    greeting: 'Hello World!',
+    user: 'Hassan Djirdeh',
+    city: 'Toronto',
+  },
+});
